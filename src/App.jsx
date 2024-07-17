ec67ce1b3c56669e4ed1aaf45827d7597a3684ac
Mark Cherenov <user@MacBook-Pro-user.local>
2024-07-17 20:14:33 +0300

    setup rkq query

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
 import Navbar from './components/UI/navbar/Navbar'
-import { useFetching } from './hooks/useFetching'
-import CharacterService from './API/CharacterService'
 import { useDispatch } from 'react-redux'
 import { setCharactersData } from './store/slices/charactersSlice'
-import { getArrayToLocalStorage } from './helpers/string'
+import { getArrayToLocalStorage } from './helpers/lib/localStorage.js'
+import { useCharacters } from "./store/services/characterApi.js";
 
 function App() {
   const dispatch = useDispatch()
   const isLocalCharsData = getArrayToLocalStorage('localCharsData')
 
-  const [getChars, isLoading, error] = useFetching(async () => {
-    const response = await CharacterService.getCharacter("characters")
-    dispatch(setCharactersData(isLocalCharsData ? isLocalCharsData : response.data))
+  const {data, isLoading, error} = useCharacters(null, {
+    pollInterval: 1000 // запросы каждые 1 сек
   })
-  
 
   useEffect(() => {
-      getChars()
-  }, []) 
+    if (!isLocalCharsData && data) {
+      dispatch(setCharactersData(data))
+    } else {
+      dispatch(setCharactersData(isLocalCharsData))
+    }
+  }, [dispatch, data])
 
   return (
     <div className='App'>
